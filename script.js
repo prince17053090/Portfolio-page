bed2974f30534817b5d6d715efe88821487fac27
Prince Vora <princevora01@gmail.com>
2024-08-30 17:02:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let contact = document.querySelector('#contact');
+
+function showform() {
+    document.querySelector('.contact-form').classList.add('showform');
+}
+
+function removeform(){
+    document.querySelector('.contact-form').classList.remove('showform');
+}
